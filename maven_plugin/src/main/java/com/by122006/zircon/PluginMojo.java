48456c1b4c463ddda109ca119f950a1b9a064cde
122006 <1109108466@qq.com>
2023-09-01 14:42:55 +0800

    update

--- maven_plugin/src/main/java/com/by122006/zircon/PluginMojo.java
+++ maven_plugin/src/main/java/com/by122006/zircon/PluginMojo.java
@@ -5,17 +5,14 @@ import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
