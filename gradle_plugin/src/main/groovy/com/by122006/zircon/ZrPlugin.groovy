418254e040ba759b61ce6842f37def0ffdd8c953
122006 <1109108466@qq.com>
2023-09-09 16:12:44 +0800

    update

--- gradle_plugin/src/main/groovy/com/by122006/zircon/ZrPlugin.groovy
+++ gradle_plugin/src/main/groovy/com/by122006/zircon/ZrPlugin.groovy
@@ -7,19 +7,12 @@ import org.gradle.api.artifacts.result.DependencyResult
 import org.gradle.api.artifacts.result.ResolvedDependencyResult
 import org.gradle.api.tasks.compile.JavaCompile
 
-class ZirconExt {
-    var version = 'latest.release'
-    var tempString = true
-    var exMethod = true
-}
-
 class ZrPlugin implements Plugin<Project> {
     void apply(Project project) {
-        ZirconExt extension = project.getExtensions().create("zircon", ZirconExt)
-        project.extensions.add("zircon", extension);
         project.tasks.withType(JavaCompile) { JavaCompile it ->
-            if (project.extension.zircon.tempString) it.options.compilerArgs << "-Xplugin:ZrExMethod"
-            if (project.extension.zircon.exMethod) it.options.compilerArgs << "-Xplugin:ZrString"
+            def zircon = project.extension.zircon
+            if (zircon.exMethod == null || zircon.exMethod) it.options.compilerArgs << "-Xplugin:ZrExMethod"
+            if (zircon.tempString == null || zircon.tempString) it.options.compilerArgs << "-Xplugin:ZrString"
             if (Integer.parseInt(System.getProperty("java.version").split("\\.")[0]) >= 11) {
                 it.options.forkOptions.jvmArgs << "--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED" << "--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED" << "--add-exports=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED" << "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED" << "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED"
                 it.options.fork = true
