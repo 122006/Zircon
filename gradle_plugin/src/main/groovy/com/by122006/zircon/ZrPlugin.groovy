979ed813bafe9151d1a12a09e5da2f76cdd25c82
122006 <1109108466@qq.com>
2023-09-09 16:03:13 +0800

    update

--- gradle_plugin/src/main/groovy/com/by122006/zircon/ZrPlugin.groovy
+++ gradle_plugin/src/main/groovy/com/by122006/zircon/ZrPlugin.groovy
@@ -8,34 +8,37 @@ import org.gradle.api.artifacts.result.ResolvedDependencyResult
 import org.gradle.api.tasks.compile.JavaCompile
 
 class ZrPlugin implements Plugin<Project> {
+    class ZirconExt {
+        var version = 'latest.release'
+        var tempString = true
+        var exMethod = true
+    }
 
     void apply(Project project) {
+        ZirconExt extension = project.getExtensions().create("zircon", ZirconExt)
+        project.extensions.add("zircon", extension);
         project.tasks.withType(JavaCompile) { JavaCompile it ->
-            it.options.compilerArgs << "-Xplugin:ZrExMethod"
-            it.options.compilerArgs << "-Xplugin:ZrString"
+            if (project.extension.zircon.tempString) it.options.compilerArgs << "-Xplugin:ZrExMethod"
+            if (project.extension.zircon.exMethod) it.options.compilerArgs << "-Xplugin:ZrString"
             if (Integer.parseInt(System.getProperty("java.version").split("\\.")[0]) >= 11) {
-                it.options.forkOptions.jvmArgs
-                        << "--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED"
-                        << "--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED"
-                        << "--add-exports=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED"
-                        << "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED"
-                        << "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED"
+                it.options.forkOptions.jvmArgs << "--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED" << "--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED" << "--add-exports=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED" << "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED" << "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED"
                 it.options.fork = true
             }
         }
+        var version = project.extensions.zircon.version || "latest.release";
         project.dependencies.add("annotationProcessor"
-                , project.dependencies.create("com.github.122006.Zircon:javac:3.+"))
+                , project.dependencies.create("com.github.122006.Zircon:javac:" + version))
         project.dependencies.add("implementation"
-                , project.dependencies.create("com.github.122006.Zircon:zircon:3.+"))
+                , project.dependencies.create("com.github.122006.Zircon:zircon:" + version))
         try {
             project.dependencies.add("androidTestImplementation"
-                    , project.dependencies.create("com.github.122006.Zircon:javac:3.+"))
+                    , project.dependencies.create("com.github.122006.Zircon:javac:" + version))
         } catch (ignored) {
 
         }
         try {
             project.dependencies.add("testAnnotationProcessor"
-                    , project.dependencies.create("com.github.122006.Zircon:javac:3.+"))
+                    , project.dependencies.create("com.github.122006.Zircon:javac:" + version))
         } catch (ignored) {
 
         }
