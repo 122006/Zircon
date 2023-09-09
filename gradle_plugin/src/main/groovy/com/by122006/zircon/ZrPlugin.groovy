d1688b0b94da59b1a16011f0476f3a351a92725a
122006 <1109108466@qq.com>
2023-09-09 16:22:14 +0800

    update

--- gradle_plugin/src/main/groovy/com/by122006/zircon/ZrPlugin.groovy
+++ gradle_plugin/src/main/groovy/com/by122006/zircon/ZrPlugin.groovy
@@ -7,18 +7,25 @@ import org.gradle.api.artifacts.result.DependencyResult
 import org.gradle.api.artifacts.result.ResolvedDependencyResult
 import org.gradle.api.tasks.compile.JavaCompile
 
+class ZirconExt {
+    String version = "latest.release"
+    boolean exMethod = true
+    boolean tempString = true
+}
+
 class ZrPlugin implements Plugin<Project> {
     void apply(Project project) {
+        project.getExtensions().create("zircon", ZirconExt)
         project.tasks.withType(JavaCompile) { JavaCompile it ->
-            def zircon = project.extension.zircon
-            if (zircon.exMethod == null || zircon.exMethod) it.options.compilerArgs << "-Xplugin:ZrExMethod"
-            if (zircon.tempString == null || zircon.tempString) it.options.compilerArgs << "-Xplugin:ZrString"
+            def zircon = project.zircon
+            if (zircon == null || zircon.exMethod == null || zircon.exMethod) it.options.compilerArgs << "-Xplugin:ZrExMethod"
+            if (zircon == null || zircon.tempString == null || zircon.tempString) it.options.compilerArgs << "-Xplugin:ZrString"
             if (Integer.parseInt(System.getProperty("java.version").split("\\.")[0]) >= 11) {
                 it.options.forkOptions.jvmArgs << "--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED" << "--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED" << "--add-exports=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED" << "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED" << "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED"
                 it.options.fork = true
             }
         }
-        var version = project.extensions.zircon.version || "latest.release";
+        var version = project.zircon?.version || "latest.release";
         project.dependencies.add("annotationProcessor"
                 , project.dependencies.create("com.github.122006.Zircon:javac:" + version))
         project.dependencies.add("implementation"
