f4b2d63f11af3cfa2f17b7da0c03fc64f91aff9a
122006 <1109108466@qq.com>
2023-09-09 19:53:37 +0800

    update

--- gradle_plugin/src/main/groovy/com/by122006/zircon/ZrPlugin.groovy
+++ gradle_plugin/src/main/groovy/com/by122006/zircon/ZrPlugin.groovy
@@ -4,40 +4,34 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.compile.JavaCompile
 
-class ZirconExt {
-    String version = "latest.release"
-    boolean exMethod = true
-    boolean tempString = true
-}
 
 class ZrPlugin implements Plugin<Project> {
     void apply(Project project) {
-        project.extensions.create("zircon", ZirconExt.class)
         project.tasks.withType(JavaCompile) { JavaCompile it ->
-            def zircon = project.zircon
-            if (zircon == null || zircon.exMethod == null || zircon.exMethod) it.options.compilerArgs << "-Xplugin:ZrExMethod"
-            if (zircon == null || zircon.tempString == null || zircon.tempString) it.options.compilerArgs << "-Xplugin:ZrString"
-            if (Integer.parseInt(System.getProperty("java.version").split("\\.")[0]) >= 11) {
+            if (!project.hasProperty("zircon_tempString") || project.zircon_tempString) it.options.compilerArgs << "-Xplugin:ZrString"
+            if (!project.hasProperty("zircon_exMethod") | project.zircon_exMethod) it.options.compilerArgs << "-Xplugin:ZrExMethod"
+            if (Integer.parseInt(System.getProperty("java.version").split("\\.")[0]) >= 9) {
                 it.options.forkOptions.jvmArgs << "--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED" << "--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED" << "--add-exports=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED" << "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED" << "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED"
                 it.options.fork = true
             }
-            var version = project.zircon.version != null ? project.zircon.version : "latest.release";
-            project.dependencies.add("annotationProcessor"
+        }
+        var version = project.hasProperty("zircon_version") ? project.zircon_version : "latest.release";
+        System.out.println(version)
+        project.dependencies.add("annotationProcessor"
+                , project.dependencies.create("com.github.122006.Zircon:javac:" + version))
+        project.dependencies.add("implementation"
+                , project.dependencies.create("com.github.122006.Zircon:zircon:" + version))
+        try {
+            project.dependencies.add("androidTestImplementation"
                     , project.dependencies.create("com.github.122006.Zircon:javac:" + version))
-            project.dependencies.add("implementation"
-                    , project.dependencies.create("com.github.122006.Zircon:zircon:" + version))
-            try {
-                project.dependencies.add("androidTestImplementation"
-                        , project.dependencies.create("com.github.122006.Zircon:javac:" + version))
-            } catch (ignored) {
+        } catch (ignored) {
 
-            }
-            try {
-                project.dependencies.add("testAnnotationProcessor"
-                        , project.dependencies.create("com.github.122006.Zircon:javac:" + version))
-            } catch (ignored) {
+        }
+        try {
+            project.dependencies.add("testAnnotationProcessor"
+                    , project.dependencies.create("com.github.122006.Zircon:javac:" + version))
+        } catch (ignored) {
 
-            }
         }
 
     }
