e71014db27cf534f906fa840a0d52c5c3357261f
122006 <1109108466@qq.com>
2023-09-11 11:14:51 +0800

    update

--- gradle_plugin/src/main/groovy/com/by122006/zircon/ZrPlugin.groovy
+++ gradle_plugin/src/main/groovy/com/by122006/zircon/ZrPlugin.groovy
@@ -8,8 +8,8 @@ import org.gradle.api.tasks.compile.JavaCompile
 class ZrPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.tasks.withType(JavaCompile) { JavaCompile it ->
-            if (!project.hasProperty("zircon_tempString") || project.zircon_tempString) it.options.compilerArgs << "-Xplugin:ZrString"
-            if (!project.hasProperty("zircon_exMethod") | project.zircon_exMethod) it.options.compilerArgs << "-Xplugin:ZrExMethod"
+            if (!project.hasProperty("zircon_temp_string") || project.zircon_temp_string) it.options.compilerArgs << "-Xplugin:ZrString"
+            if (!project.hasProperty("zircon_ex_method") || project.zircon_ex_method) it.options.compilerArgs << "-Xplugin:ZrExMethod"
             if (Integer.parseInt(System.getProperty("java.version").split("\\.")[0]) >= 9) {
                 it.options.forkOptions.jvmArgs << "--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED" << "--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED" << "--add-exports=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED" << "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED" << "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED"
                 it.options.fork = true
